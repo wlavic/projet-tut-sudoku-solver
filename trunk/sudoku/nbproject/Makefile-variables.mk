1f567ad056ca5ff00b6504dba2f5337fffb02915
pesseyjulien@gmail.com <pesseyjulien@gmail.com@4a2f471e-a96f-f7b7-86a0-df1823e1023a>
2011-03-25 14:22:48 +0000

    Source

---
+++ trunk/sudoku/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=None-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/None-Windows
+CND_ARTIFACT_NAME_Debug=sudoku
+CND_ARTIFACT_PATH_Debug=dist/Debug/None-Windows/sudoku
+CND_PACKAGE_DIR_Debug=dist/Debug/None-Windows/package
+CND_PACKAGE_NAME_Debug=sudoku.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/None-Windows/package/sudoku.tar
+# Release configuration
+CND_PLATFORM_Release=None-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/None-Windows
+CND_ARTIFACT_NAME_Release=sudoku
+CND_ARTIFACT_PATH_Release=dist/Release/None-Windows/sudoku
+CND_PACKAGE_DIR_Release=dist/Release/None-Windows/package
+CND_PACKAGE_NAME_Release=sudoku.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-Windows/package/sudoku.tar
